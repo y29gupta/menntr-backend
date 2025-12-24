033f7e1ec2c57b3993b3fe075801c10cb14a7d6c
Vijayakumar-Harish <vijayakumarharisha@gmail.com>
2025-12-24 18:05:44 +0530

    fix: change password handler update

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,6 +7,7 @@ import {
   changePasswordHandler,
   logoutHandler,
 } from '../controllers/auth.controller';
+import { authGuard } from '../hooks/auth.guard';
 
 export default async function authRoutes(fastify: FastifyInstance) {
   fastify.post('/login', loginHandler);
@@ -14,7 +15,7 @@ export default async function authRoutes(fastify: FastifyInstance) {
   fastify.post('/invite', generateInviteHandler);
   fastify.post('/consume-invite', consumeInviteHandler);
   fastify.post(
-    '/change-password',
+    '/change-password', {preHandler:authGuard},
     changePasswordHandler
   );
 }
